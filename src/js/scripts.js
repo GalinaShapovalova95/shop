bbc0e20dbda27023ccc38486228d94c8bc065173
Galina <shapovalova9595@mail.ru>
2017-10-24 00:04:39 +0300

    adaptive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,6 +14,7 @@ const greenColor = document.getElementById('col3');
 
 whiteColor.onclick = function () {
     document.getElementById('main-photo').src='img/tshirt_white.jpg';
+    document.getElementById('col1').classList.remove('props-product__item_active');
     document.getElementById('col2').classList.remove('props-product__item_active');
     document.getElementById('col3').classList.remove('props-product__item_active');
 
@@ -23,6 +24,7 @@ whiteColor.onclick = function () {
 yellowColor.onclick = function (){
     document.getElementById('main-photo').src='img/tshirt_yellow.jpg';
     document.getElementById('col1').classList.remove('props-product__item_active');
+    document.getElementById('col2').classList.remove('props-product__item_active');
     document.getElementById('col3').classList.remove('props-product__item_active');
 
     document.getElementById('col2').classList.add('props-product__item_active');
@@ -31,6 +33,7 @@ greenColor.onclick = function(){
     document.getElementById('main-photo').src='img/tshirt_green.jpg';
     document.getElementById('col1').classList.remove('props-product__item_active');
     document.getElementById('col2').classList.remove('props-product__item_active');
+    document.getElementById('col3').classList.remove('props-product__item_active');
 
     document.getElementById('col3').classList.add('props-product__item_active');
 };
