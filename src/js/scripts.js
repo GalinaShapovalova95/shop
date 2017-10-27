7b5ff18c658a77ebf22fe6910bddb60fa73b369d
Galina <shapovalova9595@mail.ru>
2017-10-27 23:02:34 +0300

    Test EJS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,33 @@
 'use strict';
 
-function changeSize(id) {
-    document.getElementById('size1').classList.remove('props-product__item_active');
-    document.getElementById('size2').classList.remove('props-product__item_active');
-    document.getElementById('size3').classList.remove('props-product__item_active');
+const sizeS = document.getElementById('size1');
+const sizeM = document.getElementById('size2');
+const sizeL = document.getElementById('size3');
 
-    document.getElementById(`${id}`).classList.add('props-product__item_active');
-}
+sizeS.onclick = function () {
+    sizeS.classList.remove('props-product__item_active');
+    sizeM.classList.remove('props-product__item_active');
+    sizeL.classList.remove('props-product__item_active');
+
+    sizeS.classList.add('props-product__item_active');
+};
+
+sizeM.onclick = function () {
+    sizeS.classList.remove('props-product__item_active');
+    sizeM.classList.remove('props-product__item_active');
+    sizeL.classList.remove('props-product__item_active');
+
+    sizeM.classList.add('props-product__item_active');
+};
+
+sizeL.onclick = function () {
+    sizeS.classList.remove('props-product__item_active');
+    sizeM.classList.remove('props-product__item_active');
+    sizeL.classList.remove('props-product__item_active');
+
+    sizeL.classList.add('props-product__item_active');
+};
+
 
 const whiteColor = document.getElementById('col1');
 const yellowColor = document.getElementById('col2');
@@ -14,26 +35,26 @@ const greenColor = document.getElementById('col3');
 
 whiteColor.onclick = function () {
     document.getElementById('main-photo').src='img/tshirt_white.jpg';
-    document.getElementById('col1').classList.remove('props-product__item_active');
-    document.getElementById('col2').classList.remove('props-product__item_active');
-    document.getElementById('col3').classList.remove('props-product__item_active');
+    whiteColor.classList.remove('props-product__item_active');
+    yellowColor.classList.remove('props-product__item_active');
+    greenColor.classList.remove('props-product__item_active');
 
-    document.getElementById('col1').classList.add('props-product__item_active');
+    whiteColor.classList.add('props-product__item_active');
 };
 
 yellowColor.onclick = function (){
     document.getElementById('main-photo').src='img/tshirt_yellow.jpg';
-    document.getElementById('col1').classList.remove('props-product__item_active');
-    document.getElementById('col2').classList.remove('props-product__item_active');
-    document.getElementById('col3').classList.remove('props-product__item_active');
+    whiteColor.classList.remove('props-product__item_active');
+    yellowColor.classList.remove('props-product__item_active');
+    greenColor.classList.remove('props-product__item_active');
 
-    document.getElementById('col2').classList.add('props-product__item_active');
+    yellowColor.classList.add('props-product__item_active');
 };
 greenColor.onclick = function(){
     document.getElementById('main-photo').src='img/tshirt_green.jpg';
-    document.getElementById('col1').classList.remove('props-product__item_active');
-    document.getElementById('col2').classList.remove('props-product__item_active');
-    document.getElementById('col3').classList.remove('props-product__item_active');
+    whiteColor.classList.remove('props-product__item_active');
+    yellowColor.classList.remove('props-product__item_active');
+    greenColor.classList.remove('props-product__item_active');
 
-    document.getElementById('col3').classList.add('props-product__item_active');
+    greenColor.classList.add('props-product__item_active');
 };
