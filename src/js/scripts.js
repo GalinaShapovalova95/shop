80393919377b4a10aabcdaacb45a7f520a7b0b60
Galina <shapovalova9595@mail.ru>
2017-11-06 15:40:44 +0300

    Change script - add if & add alt for img

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,8 @@ const productImage = document.getElementById('mainPhoto');
 const dataOfClothes = document.querySelector('.product-props');
 
 dataOfClothes.addEventListener('click', function (event) {
-    //const color = event.target.getAttribute('data-color');
     const color = event.target.dataset.color;
-    productImage.src =`img/tshirt_${color}.jpg`;
+    if (color !== undefined) {
+        productImage.src = `img/tshirt_${color}.jpg`;
+    }
 });
